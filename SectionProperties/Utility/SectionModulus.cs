54183aea697fd16ed91dde3310ef0d2fb9b682b0
kpne <kpn@kpne.dk>
2024-10-23 18:06:04 +0200

    section modulus implementation

---
+++ SectionProperties/Utility/SectionModulus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MagmaWorks.Geometry;
+using MagmaWorks.Taxonomy.Profiles;
+using OasysUnits;
+using OasysUnits.Units;
+
+namespace MagmaWorks.Taxonomy.Sections.SectionProperties.Utility
+{
+    public static class SectionModulus
+    {
+        public static OasysUnits.SectionModulus CalculateSectionModulusYy(IProfile profile)
+        {
+            ILocalPoint2d elasticCentroid = Centroid.CalculateCentroid(profile);
+            ILocalDomain2d domain = Extends.GetDomain(profile);
+            return CalculateSectionModulus(profile, domain.Max.Z, domain.Min.Z, elasticCentroid.Z);
+        }
+
+        public static OasysUnits.SectionModulus CalculateSectionModulusZz(IProfile profile)
+        {
+            ILocalPoint2d elasticCentroid = Centroid.CalculateCentroid(profile);
+            ILocalDomain2d domain = Extends.GetDomain(profile);
+            return CalculateSectionModulus(profile, domain.Max.Y, domain.Min.Y, elasticCentroid.Y);
+        }
+
+        private static OasysUnits.SectionModulus CalculateSectionModulus(IProfile profile, Length dPos, Length dNeg, Length centroid)
+        {
+            AreaMomentOfInertia inertia = Inertia.CalculateInertiaYy(profile);
+            Length zpos = Distance(dPos, centroid);
+            Length zneg = Distance(dNeg, centroid);
+            Length distance = zpos > zneg ? zpos : zneg;
+            LengthUnit unit = dPos.Unit;
+            OasysUnits.SectionModulus.TryParse($"0 {Length.GetAbbreviation(unit)}³", out OasysUnits.SectionModulus m3);
+            AreaMomentOfInertia.TryParse($"0 {Length.GetAbbreviation(unit)}⁴", out AreaMomentOfInertia m4);
+            return new OasysUnits.SectionModulus(inertia.As(m4.Unit) / distance.As(unit), m3.Unit);
+        }
+
+        private static Length Distance(Length d1, Length d2)
+        {
+            LengthUnit unit = d1.Unit;
+            return new Length(Math.Abs(d1.As(unit) - d2.As(unit)), unit);
+        }
+    }
+}

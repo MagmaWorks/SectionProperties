d8eb739b56231882e202f87f4839154514951cab
kpne <kpn@kpne.dk>
2024-11-06 18:24:38 +0100

    move geometrical properties to sectionproperties and have concreteproperties extend that instead of separate class

---
+++ ISectionProperties/IConcreteProperties.cs
@@ -0,0 +1,15 @@
+﻿using OasysUnits;
+
+namespace MagmaWorks.Taxonomy.Sections.SectionProperties
+{
+    public interface IConcreteProperties : ISectionProperties
+    {
+        Length EffectiveDepthY { get; }
+        Length EffectiveDepthZ { get; }
+        Area CompressionReinforcementAreaY { get; }
+        Area CompressionReinforcementAreaZ { get; }
+        Area TensionReinforcementAreaY { get; }
+        Area TensionReinforcementAreaZ { get; }
+        Area ShearReinforcementArea { get; }
+    }
+}
--- SectionProperties/SectionProperties.cs
+++ SectionProperties/SectionProperties.cs
@@ -1,16 +1,44 @@
-﻿using MagmaWorks.Taxonomy.Profiles;
+﻿using MagmaWorks.Geometry;
+using MagmaWorks.Taxonomy.Profiles;
+using OasysUnits;
 
 namespace MagmaWorks.Taxonomy.Sections.SectionProperties
 {
     public class SectionProperties : ISectionProperties
     {
-        public IGeometricalProperties GeometricalProperties
-            => _geometricalProperties ??= new GeometricalProperties(_profile);
+        public ILocalPoint2d Centroid
+            => _centroid ??= Utility.Centroid.CalculateCentroid(_profile);
+        public Length Perimeter
+            => _perimeter ??= Utility.PerimeterLength.CalculatePerimeter(_profile);
+        public ILocalDomain2d Extends => _domain ??= Utility.Extends.GetDomain(_profile);
+        public Area Area => _area ??= Utility.Area.CalculateArea(_profile);
+        public SectionModulus ElasticSectionModulusYy
+            => _elasticSectionModulusYy ??= Utility.SectionModulus.CalculateSectionModulusYy(_profile);
+        public SectionModulus ElasticSectionModulusZz
+            => _elasticSectionModulusZz ??= Utility.SectionModulus.CalculateSectionModulusZz(_profile);
+        public AreaMomentOfInertia MomentOfInertiaYy
+            => _momentOfInertiaYy ??= Utility.Inertia.CalculateInertiaYy(_profile);
+        public AreaMomentOfInertia MomentOfInertiaZz
+            => _momentOfInertiaZz ??= Utility.Inertia.CalculateInertiaZz(_profile);
+        public Length RadiusOfGyrationYy
+            => _radiusOfGyrationYy ??= Utility.RadiusOfGyration.CalculateRadiusOfGyrationYy(_profile);
+        public Length RadiusOfGyrationZz
+            => _radiusOfGyrationZz ??= Utility.RadiusOfGyration.CalculateRadiusOfGyrationZz(_profile);
 
-        private IGeometricalProperties _geometricalProperties;
+        private ILocalPoint2d _centroid;
+        private Length? _perimeter;
+        private ILocalDomain2d _domain;
+        private Area? _area;
+        private SectionModulus? _elasticSectionModulusYy;
+        private SectionModulus? _elasticSectionModulusZz;
+        private AreaMomentOfInertia? _momentOfInertiaYy;
+        private AreaMomentOfInertia? _momentOfInertiaZz;
+        private Length? _radiusOfGyrationYy;
+        private Length? _radiusOfGyrationZz;
         private IProfile _profile;
 
         private SectionProperties() { }
+
         public SectionProperties(ISection section) : this(section.Profile) { }
 
         public SectionProperties(IProfile profile)

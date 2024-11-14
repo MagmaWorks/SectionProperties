731054b4223394b61011c147f3dca755a1a901bb
kpne <kpn@kpne.dk>
2024-11-14 17:48:58 +0100

    concrete section and reinforcement tests

---
+++ SectionPropertiesTests/ConcreteSectionPropertiesTests.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Reflection;
+using MagmaWorks.Taxonomy.Sections;
+using MagmaWorks.Taxonomy.Sections.SectionProperties;
+using MagmaWorks.Taxonomy.Serialization.Sections.SectionProperties.Extensions;
+using SectionPropertiesTests.TestUtility;
+
+namespace SectionPropertiesTests
+{
+    public class ConcreteSectionPropertiesTests
+    {
+        [Fact]
+        public void ReinforcementAreaTest()
+        {
+            // Assemble
+            ConcreteSection setion = ConcreteSections.Perimeter();
+
+            // Act
+            var props = new ConcreteSectionProperties(setion);
+
+            // Assert
+            double expected = 2 * Math.PI / 4 * Math.Pow(12, 2)
+                + 4 * Math.PI / 4 * Math.Pow(20, 2);
+            Assert.Equal(expected, props.ReinforcemenArea.SquareMillimeters, 9);
+        }
+
+        [Fact]
+        public void ConcreteAreaTest()
+        {
+            // Assemble
+            ConcreteSection setion = ConcreteSections.Rectangle();
+
+            // Act
+            var props = new ConcreteSectionProperties(setion);
+
+            // Assert
+            double expectedReinforcementArea = 2 * Math.PI / 4 * Math.Pow(12, 2)
+                + 4 * Math.PI / 4 * Math.Pow(20, 2);
+            double expected = 400 * 750 - expectedReinforcementArea;
+            Assert.Equal(expected, props.ConcreteArea.SquareMillimeters, 9);
+        }
+
+        [Fact]
+        public void GeometricReinforcementRatioTest()
+        {
+            // Assemble
+            ConcreteSection setion = ConcreteSections.Perimeter();
+
+            // Act
+            var props = new ConcreteSectionProperties(setion);
+
+            // Assert
+            double expectedReinforcementArea = 2 * Math.PI / 4 * Math.Pow(12, 2)
+                + 4 * Math.PI / 4 * Math.Pow(20, 2);
+            double expectedConcreteArea = 400 * 750 - expectedReinforcementArea;
+            double expected = expectedReinforcementArea / expectedConcreteArea;
+            Assert.Equal(expected, props.GeometricReinforcementRatio.DecimalFractions, 9);
+        }
+
+        [Fact]
+        public void ShearReinforcementAreaTest()
+        {
+            // Assemble
+            ConcreteSection setion = ConcreteSections.Rectangle();
+
+            // Act
+            var props = new ConcreteSectionProperties(setion);
+
+            // Assert
+            double expected = 2 * Math.PI / 4 * Math.Pow(10, 2);
+            Assert.Equal(expected, props.ShearReinforcementArea.SquareMillimeters, 9);
+        }
+
+        [Fact]
+        public void ReinforcementSecondMomentOfAreaYyTest()
+        {
+            // Assemble
+            ConcreteSection setion = ConcreteSections.Perimeter();
+
+            // Act
+            var props = new ConcreteSectionProperties(setion);
+
+            // Assert
+            double expected = 2 * (Math.PI / 64 * Math.Pow(12, 4)
+                + (Math.PI / 4 * Math.Pow(12, 2) * Math.Pow(334, 2)));
+            expected += 4 * (Math.PI / 64 * Math.Pow(20, 4)
+                + (Math.PI / 4 * Math.Pow(20, 2) * Math.Pow(330, 2)));
+            Assert.Equal(expected, props.ReinforcementSecondMomentOfAreaYy.MillimetersToTheFourth, 9);
+        }
+
+        [Fact]
+        public void ReinforcementSecondMomentOfAreaZzTest()
+        {
+            // Assemble
+            ConcreteSection setion = ConcreteSections.Rectangle();
+
+            // Act
+            var props = new ConcreteSectionProperties(setion);
+
+            // Assert
+            double expected = 2 * (Math.PI / 64 * Math.Pow(12, 4)
+                + (Math.PI / 4 * Math.Pow(12, 2) * Math.Pow(159, 2)));
+            expected += 2 * (Math.PI / 64 * Math.Pow(20, 4)
+                + (Math.PI / 4 * Math.Pow(20, 2) * Math.Pow(155, 2)));
+            expected += 2 * (Math.PI / 64 * Math.Pow(20, 4)
+                + (Math.PI / 4 * Math.Pow(20, 2) * Math.Pow(51.666667, 2)));
+            Assert.Equal(expected, props.ReinforcementSecondMomentOfAreaZz.MillimetersToTheFourth, 9);
+        }
+
+        [Fact]
+        public void ReinforcementRadiusOfGyrationYyTest()
+        {
+            // Assemble
+            ConcreteSection setion = ConcreteSections.Perimeter();
+
+            // Act
+            var props = new ConcreteSectionProperties(setion);
+
+            // Assert
+            double expected = 2 * (Math.PI / 64 * Math.Pow(12, 4)
+                + (Math.PI / 4 * Math.Pow(12, 2) * Math.Pow(334, 2)));
+            expected += 4 * (Math.PI / 64 * Math.Pow(20, 4)
+                + (Math.PI / 4 * Math.Pow(20, 2) * Math.Pow(330, 2)));
+            expected /= 2 * Math.PI / 4 * Math.Pow(12, 2)
+                + 4 * Math.PI / 4 * Math.Pow(20, 2);
+            Assert.Equal(Math.Sqrt(expected), props.ReinforcementRadiusOfGyrationYy.Millimeters, 9);
+        }
+
+        [Fact]
+        public void ReinforcementRadiusOfGyrationZzTest()
+        {
+            // Assemble
+            ConcreteSection setion = ConcreteSections.Rectangle();
+
+            // Act
+            var props = new ConcreteSectionProperties(setion);
+
+            // Assert
+            double expected = 2 * (Math.PI / 64 * Math.Pow(12, 4)
+                + (Math.PI / 4 * Math.Pow(12, 2) * Math.Pow(159, 2)));
+            expected += 2 * (Math.PI / 64 * Math.Pow(20, 4)
+                + (Math.PI / 4 * Math.Pow(20, 2) * Math.Pow(155, 2)));
+            expected += 2 * (Math.PI / 64 * Math.Pow(20, 4)
+                + (Math.PI / 4 * Math.Pow(20, 2) * Math.Pow(51.666667, 2)));
+            expected /= 2 * Math.PI / 4 * Math.Pow(12, 2)
+                + 4 * Math.PI / 4 * Math.Pow(20, 2);
+            Assert.Equal(Math.Sqrt(expected), props.ReinforcementRadiusOfGyrationZz.Millimeters, 9);
+        }
+    }
+}
--- SectionPropertiesTests/TestUtility/ConcreteSections.cs
+++ SectionPropertiesTests/TestUtility/ConcreteSections.cs
@@ -1,12 +1,88 @@
 ﻿using MagmaWorks.Geometry;
 using MagmaWorks.Taxonomy.Profiles;
+using MagmaWorks.Taxonomy.Sections;
 using OasysUnits.Units;
 using OasysUnits;
+using MagmaWorks.Taxonomy.Materials;
 
 namespace SectionPropertiesTests.TestUtility
 {
     internal static class ConcreteSections
     {
+        internal static ConcreteSection Perimeter()
+        {
+            LengthUnit unit = LengthUnit.Millimeter;
+            IMaterial material = new MockMaterial();
+            IProfile profile = PerimeterRectangle400x750();
+            double link = 10;
+            double cover = 25;
+            var rebars = new List<ILongitudinalReinforcement>()
+            {
+                Rebar(12, -200, 375, link, cover),
+                Rebar(12, 200, 375, link, cover),
+                Rebar(20, -200, -375, link, cover),
+                Rebar(20, -51.666667, -330),
+                Rebar(20, 51.666667, -330),
+                Rebar(20, 200, -375, link, cover),
+            };
+
+            return new ConcreteSection(material, profile, rebars, Bar(link), new Length(cover, unit));
+        }
+
+        internal static ConcreteSection Rectangle()
+        {
+            LengthUnit unit = LengthUnit.Millimeter;
+            IMaterial material = new MockMaterial();
+            IProfile profile = Rectangle400x750();
+            double link = 10;
+            double cover = 25;
+            var rebars = new List<ILongitudinalReinforcement>()
+            {
+                Rebar(12, -200, 375, link, cover),
+                Rebar(12, 200, 375, link, cover),
+                Rebar(20, -200, -375, link, cover),
+                Rebar(20, -51.666667, -330),
+                Rebar(20, 51.666667, -330),
+                Rebar(20, 200, -375, link, cover),
+            };
+            
+            return new ConcreteSection(material, profile, rebars, Bar(link), new Length(cover, unit));
+        }
+
+        private static LongitudinalReinforcement Rebar(double dia, double edgeY, double edgeZ, double link, double cover)
+        {
+            LengthUnit unit = LengthUnit.Millimeter;
+            double posY = edgeY > 0
+                ? edgeY - link - cover - dia / 2
+                : edgeY + link + cover + dia / 2;
+            double posZ = edgeZ > 0
+                ? edgeZ - link - cover - dia / 2
+                : edgeZ + link + cover + dia / 2;
+            return new LongitudinalReinforcement(Bar(dia),
+                    new LocalPoint2d()
+                    {
+                        Y = new Length(posY, unit),
+                        Z = new Length(posZ, unit)
+                    });
+        }
+
+        private static LongitudinalReinforcement Rebar(double dia, double posY, double posZ)
+        {
+            LengthUnit unit = LengthUnit.Millimeter;
+            return new LongitudinalReinforcement(Bar(dia),
+                    new LocalPoint2d()
+                    {
+                        Y = new Length(posY, unit),
+                        Z = new Length(posZ, unit)
+                    });
+        }
+
+        private static Rebar Bar(double dia)
+        {
+            Length diameter = new Length(dia, LengthUnit.Millimeter);
+            return new Rebar(new MockMaterial(), diameter);
+        }
+
         private static Perimeter PerimeterRectangle400x750()
         {
             LengthUnit unit = LengthUnit.Millimeter;
@@ -27,6 +103,23 @@ namespace SectionPropertiesTests.TestUtility
             Length width = new Length(400, unit);
             Length height = new Length(750, unit);
             return new Rectangle(width, height);
+        }
+
+        private static Circle CircleD550()
+        {
+            LengthUnit unit = LengthUnit.Millimeter;
+            Length diameter = new Length(550, unit);
+            return new Circle(diameter);
+        }
+
+        private static Tee Tee800x500()
+        {
+            LengthUnit unit = LengthUnit.Millimeter;
+            Length width = new Length(800, unit);
+            Length height = new Length(500, unit);
+            Length flange = new Length(150, unit);
+            Length web = new Length(90, unit);
+            return new Tee(height, width, flange, web);
         }
     }
 }

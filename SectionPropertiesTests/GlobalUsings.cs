fa565598e9ff76c5e88e03d02989828538cad712
Lint Action <lint-action@samuelmeuli.com>
2025-04-17 14:29:29 +0000

    Fix code style issues with dotnet_format

--- SectionPropertiesTests/GlobalUsings.cs
+++ SectionPropertiesTests/GlobalUsings.cs
@@ -1,6 +1,6 @@
 global using MagmaWorks.Taxonomy.Sections;
 global using MagmaWorks.Taxonomy.Sections.SectionProperties;
-global using UnitsNet;
 global using SectionPropertiesTests.TestUtility;
+global using UnitsNet;
 global using Xunit;
 global using SectionModulus = UnitsNet.Volume;
